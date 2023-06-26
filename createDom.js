28936d83fb026d301ac104509223659e19bd11b8
balazstrencsenyi <b.trencsenyi@gmail.com>
2023-06-26 21:20:52 +0200

    +

--- createDom.js
+++ createDom.js
@@ -51,15 +51,123 @@ export function createHeader() {
 
   menuButton.addEventListener("click", toggleMenu);
 
+  // Define an array of menu items
+  const menuItems = [
+    { text: "Home" },
+    { text: "Education" },
+    { text: "Portfolio" },
+    { text: "Experience" },
+    { text: "Contact" },
+  ];
+
+  createMobileMenu(menuItems); // Pass the menu items to createMobileMenu
+
   return header;
 }
+let mobileMenuVisible = false; // Track the visibility state of the mobile menu
+let mobileMenu = null; // Track the reference to the mobile menu element
 
 function toggleMenu() {
-  // Logic to toggle the menu display
+  if (mobileMenu) {
+    // If the mobile menu already exists, toggle its visibility
+    if (mobileMenuVisible) {
+      mobileMenu.style.display = "none"; // Hide the mobile menu
+    } else {
+      mobileMenu.style.display = "block"; // Show the mobile menu
+    }
+    mobileMenuVisible = !mobileMenuVisible; // Toggle the visibility state
+  } else {
+    // If the mobile menu doesn't exist, create it
+    mobileMenu = createMobileMenu();
+    mobileMenuVisible = true; // Set the visibility state to true since it's being created
+  }
+}
+
+export function createMobileMenu() {
+  const mobileMenu = createEl("div");
+  mobileMenu.setAttribute("id", "mobile-menu");
+
+  const mobileMenuEl1 = createEl("div");
+  mobileMenuEl1.classList.add("mobile-menu-el");
+  mobileMenuEl1.innerText = "Home";
+  mobileMenuEl1.id = "upper-item-mobile";
+
+  const mobileMenuEl2 = createEl("div");
+  mobileMenuEl2.classList.add("mobile-menu-el");
+  mobileMenuEl2.innerText = "Education";
+
+  const mobileMenuEl3 = createEl("div");
+  mobileMenuEl3.classList.add("mobile-menu-el");
+  mobileMenuEl3.innerText = "Portfolio";
+
+  const mobileMenuEl4 = createEl("div");
+  mobileMenuEl4.classList.add("mobile-menu-el");
+  mobileMenuEl4.innerText = "Experience";
+
+  const mobileMenuEl5 = createEl("div");
+  mobileMenuEl5.classList.add("mobile-menu-el");
+  mobileMenuEl5.innerText = "Contact";
+  mobileMenuEl5.id = "lower-item-mobile";
+
+  mobileMenu.append(
+    mobileMenuEl1,
+    mobileMenuEl2,
+    mobileMenuEl3,
+    mobileMenuEl4,
+    mobileMenuEl5
+  );
+  root.append(mobileMenu);
+
+  mobileMenuEl1.addEventListener("click", () => handleMobileMenuClick("Home"));
+  mobileMenuEl2.addEventListener("click", () =>
+    handleMobileMenuClick("Education")
+  );
+  mobileMenuEl3.addEventListener("click", () =>
+    handleMobileMenuClick("Portfolio")
+  );
+  mobileMenuEl4.addEventListener("click", () =>
+    handleMobileMenuClick("Experience")
+  );
+  mobileMenuEl5.addEventListener("click", () =>
+    handleMobileMenuClick("Contact")
+  );
+
+  return mobileMenu;
+}
+
+function handleMobileMenuClick(selectedNavItem) {
+  if (mobileMenuVisible) {
+    toggleMenu(); // Hide the mobile menu
+  }
+
+  root.classList.add("slide-out"); // Apply the slide-out animation
+
+  setTimeout(() => {
+    root.innerHTML = "";
+
+    if (selectedNavItem === "Home") {
+      createMain();
+    } else if (selectedNavItem === "Education") {
+      createEducation();
+    } else if (selectedNavItem === "Portfolio") {
+      createPortfolio();
+    } else if (selectedNavItem === "Experience") {
+      createExperience();
+    } else if (selectedNavItem === "Contact") {
+      createContact();
+    }
+
+    root.classList.remove("slide-out");
+    root.classList.add("slide-in"); // Apply the slide-in animation
+
+    setTimeout(() => {
+      root.classList.remove("slide-in");
+    }, 500); // Adjust the timeout duration to match the animation duration in CSS
+  }, 500); // Adjust the timeout duration to match the animation duration in CSS
 }
 
 
-export function buttonHandler() {
+export function buttonHandler(element) {
   const navEls = document.querySelectorAll(".navEl");
 
   navEls.forEach((navEl) => {
@@ -129,6 +237,8 @@ export function createNavBar() {
   navBar.append(navEl1, navEl2, navEl3, navEl4, navEl5);
   root.append(navBar);
 
+  buttonHandler(); // Call the buttonHandler function to set up event listeners for navigation items
+
   return navBar;
 }
 
@@ -153,8 +263,8 @@ export function createMain() {
 
   root.innerHTML = ""; // Clear root content
 
-    body.style.backgroundColor = "rgb(0, 128, 255)";
-    root.style.backgroundColor = "rgb(0, 128, 255)";
+  body.style.backgroundColor = "rgb(0, 128, 255)";
+  root.style.backgroundColor = "rgb(0, 128, 255)";
 
   root.append(main); // Rebuild the content
 
@@ -177,12 +287,12 @@ export function createEducation() {
   educationText.innerText =
     "I have completed my Bachelor's degree in Budapest University of Technology and Economics. To further enhance my skill set and pursue my passion for frontend development, I attended a Frontend Developer school at CodeCool. During this program, I received comprehensive training in various technologies and frameworks, including HTML, CSS, JavaScript, TypeScript, React, Node.js, Express, and REST API development.Through my education and practical experience, I have developed a solid understanding of frontend development principles and techniques. I am proficient in creating responsive and user-friendly web interfaces using HTML and CSS. I have a strong command of JavaScript, enabling me to develop interactive and dynamic web applications. Moreover, I am experienced in working with popular frontend frameworks such as React, which enables me to build efficient and modular applications. I have also gained expertise in backend development using Node.js and Express, allowing me to create robust server-side applications and RESTful APIs.Overall, my education and training have equipped me with a diverse skill set and a deep understanding of frontend development and related technologies. I am excited to apply my knowledge and contribute to impactful projects in the field.";
 
-  educationPage.append(educationTitle,educationText);
+  educationPage.append(educationTitle, educationText);
 
   // Clear the existing content inside root
   root.innerHTML = "";
-    root.style.backgroundColor = "rgba(0, 130, 86, 1)";
-    body.style.backgroundColor = "rgba(0, 130, 86, 1)";
+  root.style.backgroundColor = "rgba(0, 130, 86, 1)";
+  body.style.backgroundColor = "rgba(0, 130, 86, 1)";
 
   root.append(educationPage); // Rebuild the content
 
@@ -202,20 +312,20 @@ export function createPortfolio() {
 
   const portfolioText2 = createEl("div");
   portfolioText2.classList.add("portfolio-text");
-  portfolioText2.innerText = "This is an ongoing private project that primarily utilizes Vanilla JavaScript, modular components, and incorporates some Node.js functionality for form implementation.";
+  portfolioText2.innerText =
+    "This is an ongoing private project that primarily utilizes Vanilla JavaScript, modular components, and incorporates some Node.js functionality for form implementation.";
 
   portfolioPage.append(portfolioText, portfolioText2);
 
   root.innerHTML = ""; // Clear root content
   root.style.height = "100vh";
 
-    body.style.backgroundColor = "rgba(138, 51, 237, 1)";
-    root.style.backgroundColor = "rgba(138, 51, 237, 1)";
-    
+  body.style.backgroundColor = "rgba(138, 51, 237, 1)";
+  root.style.backgroundColor = "rgba(138, 51, 237, 1)";
 
   root.append(portfolioPage); // Rebuild the content
 
-  buttonHandler();
+    buttonHandler();
 
   return portfolioPage;
 }
@@ -226,75 +336,95 @@ export function createExperience() {
 
   const experienceText = createEl("div");
   experienceText.classList.add("experience-text");
-  experienceText.innerText =
-    "My Experiences ";
+  experienceText.innerText = "My Experiences ";
 
-    const sapContainer = createEl("div");
-    sapContainer.classList.add("sap-container");
+  const sapContainer = createEl("div");
+  sapContainer.classList.add("sap-container");
 
-    const experienceDate = createEl("div");
-    experienceDate.classList.add("experience-date");
-    experienceDate.innerText = "2021 - 2022";
+  const experienceDate = createEl("div");
+  experienceDate.classList.add("experience-date");
+  experienceDate.innerText = "2021 - 2022";
 
-    const experienceTitle = createEl("div");
-    experienceTitle.classList.add("experience-title");
-    experienceTitle.innerText = "SAP Business Analyst";
+  const experienceTitle = createEl("div");
+  experienceTitle.classList.add("experience-title");
+  experienceTitle.innerText = "SAP Business Analyst";
 
-    const experienceCompany = createEl("div");
-    experienceCompany.classList.add("experience-company");
-    experienceCompany.innerText = "KPMG";
+  const experienceCompany = createEl("div");
+  experienceCompany.classList.add("experience-company");
+  experienceCompany.innerText = "KPMG";
 
-    const sapDetails = createEl("div");
-    sapDetails.classList.add("sap-details");
-    sapDetails.innerText = "Over these Period, I had been on a major implementation project. My tasks included assessing client needs, facilitating communication between them and developers, ensuring new requests align with client requirements, and participating in client presentations. This project has allowed me to contribute to successful goal implementation with clear and consistent communication."
+  const sapDetails = createEl("div");
+  sapDetails.classList.add("sap-details");
+  sapDetails.innerText =
+    "Over these Period, I had been on a major implementation project. My tasks included assessing client needs, facilitating communication between them and developers, ensuring new requests align with client requirements, and participating in client presentations. This project has allowed me to contribute to successful goal implementation with clear and consistent communication.";
 
-    sapContainer.append(experienceDate, experienceTitle, experienceCompany, sapDetails);
+  sapContainer.append(
+    experienceDate,
+    experienceTitle,
+    experienceCompany,
+    sapDetails
+  );
 
-    const deloitteContainer = createEl("div");
-    deloitteContainer.classList.add("deloitte-container");
+  const deloitteContainer = createEl("div");
+  deloitteContainer.classList.add("deloitte-container");
 
-    const experienceDate2 = createEl("div");
-    experienceDate2.classList.add("experience-date");
-    experienceDate2.innerText = "2022";
+  const experienceDate2 = createEl("div");
+  experienceDate2.classList.add("experience-date");
+  experienceDate2.innerText = "2022";
 
-    const experienceTitle2 = createEl("div");
-    experienceTitle2.classList.add("experience-title");
-    experienceTitle2.innerText = "Audit Analyst";
+  const experienceTitle2 = createEl("div");
+  experienceTitle2.classList.add("experience-title");
+  experienceTitle2.innerText = "Audit Analyst";
 
-    const experienceCompany2 = createEl("div");
-    experienceCompany2.classList.add("experience-company");
-    experienceCompany2.innerText = "Deloitte";
+  const experienceCompany2 = createEl("div");
+  experienceCompany2.classList.add("experience-company");
+  experienceCompany2.innerText = "Deloitte";
 
-    const deloitteDetails = createEl("div");
-    deloitteDetails.classList.add("deloitte-details");
-    deloitteDetails.innerText = "As a Junior Auditor at Deloitte, I had the chance to dive deeper into the world of finance, working closely with experienced team members to analyze yearly reports. My time at Deloitte taught me the importance of integrity, goal achievement, teamwork, and familiarized me with various financial software applications"
+  const deloitteDetails = createEl("div");
+  deloitteDetails.classList.add("deloitte-details");
+  deloitteDetails.innerText =
+    "As a Junior Auditor at Deloitte, I had the chance to dive deeper into the world of finance, working closely with experienced team members to analyze yearly reports. My time at Deloitte taught me the importance of integrity, goal achievement, teamwork, and familiarized me with various financial software applications";
 
-    deloitteContainer.append(experienceDate2, experienceTitle2, experienceCompany2, deloitteDetails);
+  deloitteContainer.append(
+    experienceDate2,
+    experienceTitle2,
+    experienceCompany2,
+    deloitteDetails
+  );
 
-    const codecoolContainer = createEl("div");
-    codecoolContainer.classList.add("codecool-container");
+  const codecoolContainer = createEl("div");
+  codecoolContainer.classList.add("codecool-container");
 
-    const experienceDate3 = createEl("div");
-    experienceDate3.classList.add("experience-date");
-    experienceDate3.innerText = "2023";
+  const experienceDate3 = createEl("div");
+  experienceDate3.classList.add("experience-date");
+  experienceDate3.innerText = "2023";
 
-    const experienceTitle3 = createEl("div");
-    experienceTitle3.classList.add("experience-title");
-    experienceTitle3.innerText = "Frontend Developer";
+  const experienceTitle3 = createEl("div");
+  experienceTitle3.classList.add("experience-title");
+  experienceTitle3.innerText = "Frontend Developer";
 
-    const experienceCompany3 = createEl("div");
-    experienceCompany3.classList.add("experience-company");
-    experienceCompany3.innerText = "CodeCool";
+  const experienceCompany3 = createEl("div");
+  experienceCompany3.classList.add("experience-company");
+  experienceCompany3.innerText = "CodeCool";
 
-    const codecoolDetails = createEl("div");
-    codecoolDetails.classList.add("codecool-details");
-    codecoolDetails.innerText = "During my tenure at Codecool, I have acquired extensive experience in both individual and collaborative web development, thereby fostering my proficiency in a wide range of technologies such as HTML5, CSS3, SVG, JavaScript, React.js, Node.js / NPM, Express.js. Additionally, I have employed Git and GitHub as valuable tools for version control and collaborative work. This comprehensive course has equipped me with a solid foundation in web development, empowering me to construct dynamic websites and applications with ease. In particular, the course structure incorporated biweekly team projects, which not only honed my teamwork skills but also provided valuable opportunities to showcase our collective efforts before esteemed Mentors. Our collaborative endeavors primarily focused on real-world tasks, allowing us to actively troubleshoot and resolve complex problems as a cohesive unit. For a more in-depth review of my work, I invite you to peruse my GitHub page at: https://github.com/balazstrencsenyi."
+  const codecoolDetails = createEl("div");
+  codecoolDetails.classList.add("codecool-details");
+  codecoolDetails.innerText =
+    "During my tenure at Codecool, I have acquired extensive experience in both individual and collaborative web development, thereby fostering my proficiency in a wide range of technologies such as HTML5, CSS3, SVG, JavaScript, React.js, Node.js / NPM, Express.js. Additionally, I have employed Git and GitHub as valuable tools for version control and collaborative work. This comprehensive course has equipped me with a solid foundation in web development, empowering me to construct dynamic websites and applications with ease. In particular, the course structure incorporated biweekly team projects, which not only honed my teamwork skills but also provided valuable opportunities to showcase our collective efforts before esteemed Mentors. Our collaborative endeavors primarily focused on real-world tasks, allowing us to actively troubleshoot and resolve complex problems as a cohesive unit. For a more in-depth review of my work, I invite you to peruse my GitHub page at: https://github.com/balazstrencsenyi.";
 
-    codecoolContainer.append(experienceDate3, experienceTitle3, experienceCompany3, codecoolDetails);
+  codecoolContainer.append(
+    experienceDate3,
+    experienceTitle3,
+    experienceCompany3,
+    codecoolDetails
+  );
 
-
-
-  experiencePage.append(experienceText, sapContainer, deloitteContainer, codecoolContainer);
+  experiencePage.append(
+    experienceText,
+    sapContainer,
+    deloitteContainer,
+    codecoolContainer
+  );
 
   root.innerHTML = ""; // Clear root content
 
@@ -314,7 +444,10 @@ export function createContact() {
   contactPage.classList.add("contact-page");
 
   const linkedinContainer = createEl("a");
-  linkedinContainer.setAttribute("href", "https://www.linkedin.com/in/balazstrencsenyi/");
+  linkedinContainer.setAttribute(
+    "href",
+    "https://www.linkedin.com/in/balazstrencsenyi/"
+  );
   linkedinContainer.classList.add("linkedin-container");
 
   const linkedinIcon = createEl("img");
@@ -353,11 +486,16 @@ export function createContact() {
 
   emailContainer.append(emailIcon);
 
-  contactPage.append(linkedinContainer, githubContainer, phoneContainer, emailContainer);
+  contactPage.append(
+    linkedinContainer,
+    githubContainer,
+    phoneContainer,
+    emailContainer
+  );
 
   root.innerHTML = ""; // Clear root content
-    body.style.backgroundColor = "RGBA(71, 77, 191, 1)";
-    root.style.backgroundColor = "RGBA(71, 77, 191, 1)";
+  body.style.backgroundColor = "RGBA(71, 77, 191, 1)";
+  root.style.backgroundColor = "RGBA(71, 77, 191, 1)";
 
   root.style.height = "100vh";
   root.append(contactPage); // Rebuild the content
@@ -372,7 +510,10 @@ export function createRightNav() {
   rightNav.classList.add("right-nav");
 
   const rightNavEl1Container = createEl("a");
-  rightNavEl1Container.setAttribute("href", "https://www.linkedin.com/in/balazstrencsenyi/");
+  rightNavEl1Container.setAttribute(
+    "href",
+    "https://www.linkedin.com/in/balazstrencsenyi/"
+  );
   rightNavEl1Container.classList.add("right-nav-el-container");
 
   const rightNavEl1 = createEl("img");
@@ -382,7 +523,10 @@ export function createRightNav() {
   rightNavEl1Container.append(rightNavEl1);
 
   const rightNavEl2Container = createEl("a");
-  rightNavEl2Container.setAttribute("href", "https://github.com/balazstrencsenyi");
+  rightNavEl2Container.setAttribute(
+    "href",
+    "https://github.com/balazstrencsenyi"
+  );
   rightNavEl2Container.classList.add("right-nav-el-container");
   rightNavEl2Container.id = "right-nav-el2-container";
 
